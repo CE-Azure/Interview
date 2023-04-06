32f8c87f624bd9cc354281c5d8dd38430d24d873
CE-Azure <130055466+CE-Azure@users.noreply.github.com>
2023-04-06 12:06:07 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "deployable_name" {
+  type = string
+}
+
+variable "deployable_version" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
