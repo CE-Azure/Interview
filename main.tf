59593e96f435bd4150b5cb142fc6a899d249b59b
CE-Azure <130055466+CE-Azure@users.noreply.github.com>
2023-04-06 12:04:35 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "azurerm" {
+  features {}
+}
+
+module "resource_group" {
+  application_name   = ""
+  business_unit      = local.business_unit
+  deployable_name    = var.deployable_name
+  deployable_version = var.deployable_version
+  environment        = var.environment
+  identifier         = local.identifier
+  location           = var.location
+  project_code       = local.project_code
+}
