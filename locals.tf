2e249a4bdef03760e32a620176115866cafe4e28
CE-Azure <130055466+CE-Azure@users.noreply.github.com>
2023-04-06 12:07:58 +0100

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,10 +1,4 @@
 locals {
-
-  tags = {
-    ApplicationName   = local.application_name
-    DeployableName    = var.deployable_name
-    DeployableVersion = var.deployable_version
-    Environment       = var.environment
-    ProjectCode       = local.project_code
-  }
+  business_unit    = "DevOps"
+  application_name = "Terraform"
 }
